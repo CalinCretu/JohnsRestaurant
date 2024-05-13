055185758881d5f425d1cbdecf6a3568a217faa8
CalinCretu <calincretu1412@gmail.com>
2024-05-13 11:52:59 +0200

    ADD - wines_db

--- src/components/pages/Vini.vue
+++ src/components/pages/Vini.vue
@@ -1,14 +1,43 @@
 <script>
+import axios from 'axios';
 
 export default {
-
-}
+  data() {
+    return {
+      viniData: null
+    };
+  },
+  created() {
+    this.fetchViniData();
+  },
+  methods: {
+    async fetchViniData() {
+      try {
+        const response = await axios.get('db.json');
+        this.viniData = response.data.vini[0]; // Accedi al primo elemento dell'array "menu"
+      } catch (error) {
+        console.error('Error fetching vini data:', error);
+      }
+    }
+  }
+};
 </script>
 
 <template>
   <div class="container-lg">
     <div class="title">
       Lista dei vini
+    </div>
+    <div v-if="viniData">
+      <ul class="container-sm">
+        <li class="sections">
+          <h1 class="groups">Al Calice</h1>
+        <li class="wines" v-for="vinoAlCalice in viniData.al_calice" :key="vinoAlCalice.name">
+
+
+        </li>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
