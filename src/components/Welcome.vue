94df4163ee2dee77043670addcfe9fbe5d658859
CalinCretu <calincretu1412@gmail.com>
2024-05-14 11:55:43 +0200

    ADD - welcome responsive

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -18,7 +18,7 @@ export default {
 <template>
   <div class="container-lg">
     <div class="hero-title">
-      John's Restaurant
+      <h1>John's Restaurant</h1>
     </div>
     <div class="second-title">
       Benvenuti
@@ -34,19 +34,41 @@ export default {
     display: flex;
     align-items: end;
     justify-content: center;
-    font-size: 90px;
+    font-size: 50px;
     text-transform: uppercase;
     font-weight: 500;
     background-image: url("/public/imgs/photos/PanoramaTodi.png");
     height: 400px;
     background-size: cover;
     background-position: top center;
+
+    h1 {
+      text-shadow: black 0px 0px 10px;
+    }
   }
 
   .second-title {
     color: black;
     font-size: 30px;
     padding-top: 15px;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .container-lg {
+    padding: 0px 0px;
+
+    .hero-title {
+      font-size: 25px;
+      align-items: start;
+      height: 300px;
+      background-image: url("/public/imgs/photos/RestaurantFoto.jpeg");
+      background-position: center;
+
+      h1 {
+        text-shadow: black 0px 0px 10px;
+      }
+    }
   }
 }
 </style>
