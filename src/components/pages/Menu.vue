1c8b5f1691d68cf886522c9fdf7ce266def63744
CalinCretu <calincretu1412@gmail.com>
2024-05-19 15:05:51 +0200

    ADD - no onlineDb version

--- src/components/pages/Menu.vue
+++ src/components/pages/Menu.vue
@@ -4,21 +4,182 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      menuData: null
+      menuData: {
+        antipasti: [
+          {
+            name: "Antipasto Misto della Valle",
+            description: "Salumi locali, bruschette e formaggi",
+            eng_description: "Local mixed Appetizer (local cured meat, bruschetta and mixed cheese)",
+            price: "16.00"
+          },
+          {
+            name: "Insalata di Polpo, Ceci e Olive Taggiasche",
+            description: "Octopus, Chickpeas and Taggiasche Olives",
+            eng_description: "Octopus, Chickpeas and Taggiasche Olives",
+            price: "16.00"
+          },
+          {
+            name: "Turbante di Angus Affumicato con insalata di Frutta e mozzarella di Bufala",
+            description: "Turban of smoked angus with a salad of fruit and buffalo mozzarella",
+            eng_description: "Turban of smoked angus with a salad of fruit and buffalo mozzarella",
+            price: "16.00"
+          },
+          {
+            name: "Involtino di Salmone affumicato con Peperoni in agrodolce e Salsa Tonnata",
+            description: "Smoked Salmon Roll with sweet and sour Peppers and Tuna fish sauce",
+            eng_description: "Smoked Salmon Roll with sweet and sour Peppers and Tuna fish sauce",
+            price: "16.00"
+          }
+        ],
+        primi: [
+          {
+            name: "La nostra cacio e pepe con tartufo",
+            description: "World Famous “Tagliolini cacio e pepe” black truffles, black pepper, sheep’s cheese",
+            eng_description: "World Famous “Tagliolini cacio e pepe” black truffles, black pepper, sheep’s cheese",
+            price: "18.00"
+          },
+          {
+            name: "Cuori ripieni di Burrata al tartufo di Norcia e Parmigiano Reggiano",
+            description: "Heart shaped ravioli filled with burrata cheese in a cream truffle and parmesan sauce",
+            eng_description: "Heart shaped ravioli filled with burrata cheese in a cream truffle and parmesan sauce",
+            price: "18.00"
+          },
+          {
+            name: "Tagliolini al tartufo",
+            description: "Tagliolini with local black truffles",
+            eng_description: "Tagliolini with local black truffles",
+            price: "18.00"
+          },
+          {
+            name: "Strangozzi con salsicce e funghi Porcini",
+            description: "Strangozzi with sausages and Porcini mushrooms",
+            eng_description: "Strangozzi with sausages and Porcini mushrooms",
+            price: "16.00"
+          },
+          {
+            name: "Tagliatelle con Gamberetti e Verdure piccanti",
+            description: "Tagliatelle with Prawns and Spicy Vegetables",
+            eng_description: "Tagliatelle with Prawns and Spicy Vegetables",
+            price: "16.00"
+          },
+          {
+            name: "Gnocchi al pesto di ortica, borragine e crespigno",
+            description: "Gnocchi with pesto of wild greens (nettles and borrage)",
+            eng_description: "Gnocchi with pesto of wild greens (nettles and borrage)",
+            price: "15.00"
+          }
+        ],
+        secondi: [
+          {
+            name: "Tagliata di vitello con Rucola, Olio di Ginepro e Rosmarino",
+            description: "Sliced veal Steak with Juniper Oil, Rocket salad and Rosemary",
+            eng_description: "Sliced veal Steak with Juniper Oil, Rocket salad and Rosemary",
+            price: "22.00"
+          },
+          {
+            name: "Filetto di manzo Irlandese alla griglia con Tartufo",
+            description: "Grilled Fillet of Irish beef with Black Truffles",
+            eng_description: "Grilled Fillet of Irish beef with Black Truffles",
+            price: "28.00"
+          },
+          {
+            name: "Filetto di Maiale con rucola e gorgonzola",
+            description: "Fillet of pork with gorgonzola and rocket salad",
+            eng_description: "Fillet of pork with gorgonzola and rocket salad",
+            price: "18.00"
+          },
+          {
+            name: "Scottadito di Agnello",
+            description: "Grilled lamb chops marinated in rosemary and thyme",
+            eng_description: "Grilled lamb chops marinated in rosemary and thyme",
+            price: "20.00"
+          },
+          {
+            name: "Spigola arrosto",
+            description: "Roasted Seabass",
+            eng_description: "Roasted Seabass",
+            price: "20.00"
+          },
+          {
+            name: "Baccalà con olive taggiasche, capperi, patate e ceci",
+            description: "Saltcod with olives, capers, potatoes and chickpeas",
+            eng_description: "Saltcod with olives, capers, potatoes and chickpeas",
+            price: "22.00"
+          },
+          {
+            name: "Faraona ripiena di cicoria ,scamorza e funghi porcini",
+            description: "Guinea fowl with smoked cheese porcini mushrooms and cicory",
+            eng_description: "Guinea fowl with smoked cheese porcini mushrooms and chicory",
+            price: "18.00"
+          }
+        ],
+        contorni: [
+          {
+            name: "Verdura ripassata",
+            description: "Sauteed vegetables (garlic and chili pepper)",
+            eng_description: "Sauteed vegetables (garlic and chili pepper)",
+            price: "6.00"
+          },
+          {
+            name: "Insalata Mista",
+            description: "Mixed Salad",
+            eng_description: "Mixed Salad",
+            price: "6.00"
+          },
+          {
+            name: "Patate saltate con aglio e rosmarino",
+            description: "Rosemary and Garlic sauteed potatoes",
+            eng_description: "Rosemary and Garlic sauteed potatoes",
+            price: "6.00"
+          },
+          {
+            name: "Verdure arrosto (peperoni, melanzane e zucchine)",
+            description: "Roasted vegetables",
+            eng_description: "Roasted vegetables",
+            price: "6.00"
+          },
+          {
+            name: "Panache di verdura di al timo",
+            description: "Mixed vegetable panache with thyme",
+            eng_description: "Mixed vegetable panache with thyme",
+            price: "6.00"
+          }
+        ],
+        dolci: [
+          {
+            name: "Cheesecake al cioccolato bianco e fiori d'arancio",
+            description: "White chocolate and orange blossom cheesecake",
+            eng_description: "White chocolate and orange blossom cheesecake",
+            price: "7.00"
+          },
+          {
+            name: "Torta Perugina",
+            description: "Flourless fondant chocolate cake",
+            eng_description: "Flourless fondant chocolate cake",
+            price: "7.00"
+          },
+          {
+            name: "Panna cotta alla menta con la salsa di coccolato fondente",
+            description: "Mint panna cotta with chocolate sauce",
+            eng_description: "Mint panna cotta with chocolate sauce",
+            price: "7.00"
+          }
+        ]
+      }
     };
   },
   created() {
-    this.fetchMenuData();
+    // this.fetchMenuData();
   },
   methods: {
-    async fetchMenuData() {
-      try {
-        const response = await axios.get('https://johns-restaurant-todi-db.onrender.com/menu');
-        this.menuData = response.data[0];
-      } catch (error) {
-        console.error('Error fetching menu data:', error);
-      }
-    }
+    // async fetchMenuData() {
+    //   try {
+    //     const response = await axios.get('https://johns-restaurant-todi-db.onrender.com/menu');
+    //     this.menuData = response.data[0];
+    //   } catch (error) {
+    //     console.error('Error fetching menu data:', error);
+    //   }
+    // }
   }
 };
 </script>
--- src/components/pages/Vini.vue
+++ src/components/pages/Vini.vue
@@ -4,22 +4,412 @@ import axios from 'axios';
 export default {
   data() {
     return {
-      viniData: null
+      viniData: {
+      "al_calice": [
+        {
+          "name": "Colle di Fontivecchie Umbria Rosso",
+          "price": " 6.00"
+        },
+        {
+          "name": "Colle di Fontivecchie Grechetto",
+          "price": " 6.00"
+        }
+      ],
+      "vini_rossi": [
+        {
+          "cantina_1": [
+            {
+              "c_name": "Terre dei Capitani",
+              "name": "Colle di Fontivecchie Umbria Rosso",
+              "price": " 18.00"
+            },
+            {
+              "c_name": "Terre dei Capitani",
+              "name": "Montefalco Sagrantino DOCG",
+              "price": " 40.00"
+            },
+            {
+              "c_name": "Terre dei Capitani",
+              "name": "Montefalco Rosso Riserva DOC",
+              "price": " 35.00"
+            },
+            {
+              "c_name": "Terre dei Capitani",
+              "name": "Montefalco Rosso",
+              "price": " 30.00"
+            },
+            {
+              "c_name": "Tenuta Campo al Mare",
+              "name": "Baia al Vento",
+              "price": " 60.00"
+            },
+            {
+              "c_name": "Cantina Tudernum ",
+              "name": "Fidenzio Montefalco Sagrantino DOCG",
+              "price": " 40.00"
+            },
+            {
+              "c_name": "Perticaia",
+              "name": "Montefalco Rosso Riserva",
+              "price": "45.00"
+            },
+            {
+              "c_name": "Agricola Mevante",
+              "name": "Montefalco Rosso",
+              "price": "35.00"
+            },
+            {
+              "c_name": "Colle delle Querce",
+              "name": "Cabernet Sauvignon",
+              "price": "40.00"
+            },
+            {
+              "c_name": "Tenuta Placidi",
+              "name": "Jagus",
+              "price": "45.00"
+            },
+            {
+              "c_name": "Tenuta Placidi",
+              "name": "Alias",
+              "price": "30.00"
+            },
+            {
+              "c_name": "Fattoria di Milziade Antano",
+              "name": "Montefalco Rosso Riserva DOC",
+              "price": "45.00"
+            },
+            {
+              "c_name": "Fattoria di Milziade Antano",
+              "name": "Montefalco Sagrantino DOCG",
+              "price": "50.00"
+            },
+            {
+              "c_name": "Argillae",
+              "name": "Sinuoso Umbria Rosso",
+              "price": "25.00"
+            },
+            {
+              "c_name": "Argillae",
+              "name": "Sinuoso 375 ml",
+              "price": "10.00"
+            },
+            {
+              "c_name": "Donnafugata",
+              "name": "Sedara",
+              "price": "25.00"
+            },
+            {
+              "c_name": "Donnafugata",
+              "name": "Mille e una Notte",
+              "price": "100.00"
+            },
+            {
+              "c_name": "Donnafugata",
+              "name": "Floramundi",
+              "price": "30.00"
+            },
+            {
+              "c_name": "Scacciadiavoli",
+              "name": "Montefalco Sagrantino",
+              "price": "40.00"
+            },
+            {
+              "c_name": "Peppucci",
+              "name": "Giovanni",
+              "price": "30.00"
+            },
+            {
+              "c_name": "Villa Sobrano",
+              "name": "Bapta",
+              "price": "22.00"
+            },
+            {
+              "c_name": "Villa Sobrano",
+              "name": "Bartolomeo",
+              "price": "28.00"
+            },
+            {
+              "c_name": "Roccafiore",
+              "name": "Prova d’Autore",
+              "price": "35.00"
+            },
+            {
+              "c_name": "Casale del Giglio",
+              "name": "Petit Verdot",
+              "price": "25.00"
+            },
+            {
+              "c_name": "Tenute del Cabreo",
+              "name": "Cabreo Il Borgo",
+              "price": "75.00"
+            },
+            {
+              "c_name": "Salvano",
+              "name": "Barolo",
+              "price": "60.00"
+            },
+            {
+              "c_name": "Sandonna",
+              "name": "Jovio",
+              "price": "30.00"
+            },
+            {
+              "c_name": "Sandonna",
+              "name": "Ciliegiolo",
+              "price": "30.00"
+            },
+            {
+              "c_name": "Zazzera",
+              "name": "Biccicocco",
+              "price": "35.00"
+            },
+            {
+              "c_name": "Zazzera",
+              "name": "Nerobacco",
+              "price": "22.00"
+            },
+            {
+              "c_name": "Tenuta Saragano",
+              "name": "Umbria rosso",
+              "price": "18.00"
+            },
+            {
+              "c_name": "Tenuta Saragano",
+              "name": "Montefalco rosso DOC",
+              "price": "30.00"
+            },
+            {
+              "c_name": "Tenuta Saragano",
+              "name": "Montefalco rosso riserva",
+              "price": "40.00"
+            },
+            {
+              "c_name": "Tenuta Saragano",
+              "name": "Sagrantino di Montefalco",
+              "price": "45.00"
+            },
+            {
+              "c_name": "Terre Margaritelli",
+              "name": "Miràntico",
+              "price": "25.00"
+            }
+          ]
+        }
+      ],
+      "vini_rosati": [
+        {
+          "cantina_1": [
+            {
+              "c_name": "Campo al Mare",
+              "name": "Bolgheri Rosato",
+              "price": "25.00"
+            },
+            {
+              "c_name": "Zazzera",
+              "name": "Serperosa",
+              "price": "20.00"
+            }
+          ]
+        }
+      ],
+      "vini_bianchi": [
+        {
+          "cantina_1": [
+            {
+              "c_name": "Terre dei Capitani",
+              "name": "Colle di Fontivecchie Umbria Grechetto",
+              "price": "18.00"
+            },
+            {
+              "c_name": "Agricola San Faustino",
+              "name": "Orvieto Classico",
+              "price": "22.00"
+            },
+            {
+              "c_name": "Ronco dei Folo",
+              "name": "Collio Sauvignon",
+              "price": "22.00"
+            },
+            {
+              "c_name": "Colle delle Querce",
+              "name": "Grechetto",
+              "price": "20.00"
+            },
+            {
+              "c_name": "Peppucci",
+              "name": "I Rovi",
+              "price": "30.00"
+            },
+            {
+              "c_name": "Peppucci",
+              "name": "Montorsolo",
+              "price": "28.00"
+            },
+            {
+              "c_name": "Altùris",
+              "name": "Sauvignon",
+              "price": "22.00"
+            },
+            {
+              "c_name": "Altùris",
+              "name": "Muller Thurgau",
+              "price": "22.00"
+            },
+            {
+              "c_name": "Altùris",
+              "name": "Traminer Aromatico",
+              "price": "22.00"
+            },
+            {
+              "c_name": "Altùris",
+              "name": "Ribolla Gialla",
+              "price": "22.00"
+            },
+            {
+              "c_name": "Altùris",
+              "name": "Pinot Grigio",
+              "price": "26.00"
+            },
+            {
+              "c_name": "Cantina Villa Sobrano",
+              "name": "Belisarius Grechetto Umbria IGP",
+              "price": "25.00"
+            },
+            {
+              "c_name": "Cantina Villa Sobrano",
+              "name": "Donna Sabina",
+              "price": "30.00"
+            },
+            {
+              "c_name": "Terre Margaritelli",
+              "name": "Greco di Renabianca",
+              "price": "30.00"
+            },
+            {
+              "c_name": "Argillae",
+              "name": "Orvieto Classico DOC Superiore",
+              "price": "30.00"
+            },
+            {
+              "c_name": "Argillae",
+              "name": "Orvieto Classico 275 ml",
+              "price": "10.00"
+            },
+            {
+              "c_name": "Casale del Giglio",
+              "name": "Petit Manseng",
+              "price": "20.00"
+            },
+            {
+              "c_name": "Zazzera",
+              "name": "Barbadoro",
+              "price": "25.00"
+            },
+            {
+              "c_name": "Donnafugata",
+              "name": "Passiperduti",
+              "price": "30.00"
+            },
+            {
+              "c_name": "Campo al Mare",
+              "name": "Vermentino",
+              "price": "25.00"
+            },
+            {
+              "c_name": "Tudernum",
+              "name": "Todi Bianco",
+              "price": "18.00"
+            },
+            {
+              "c_name": "Tudernum",
+              "name": "Collenobile",
+              "price": "30.00"
+            },
+            {
+              "c_name": "Tudernum",
+              "name": "Registro 209 Frizzante",
+              "price": "18.00"
+            },
+            {
+              "c_name": "Tenuta Saragano",
+              "name": "Montefalco Bianco",
+              "price": "25.00"
+            }
+          ]
+        }
+      ],
+      "prosecco_e_spumante": [
+        {
+          "cantina_1": [
+            {
+              "c_name": "Costaross",
+              "name": "Millesimato extra dry Treviso",
+              "price": "20.00"
+            },
+            {
+              "c_name": "Tenuta Saragano",
+              "name": "Segreto di famiglia Spumante Brut",
+              "price": "25.00"
+            }
+          ]
+        }
+      ],
+      "birra": [
+        {
+          "name": "Birra Grande 66 cl",
+          "price": " 7.00"
+        },
+        {
+          "name": "Birra Piccola 33 cl",
+          "price": " 4.00"
+        }
+      ],
+      "bevande": [
+        {
+          "name": "Coca-Cola 33 cl",
+          "price": " 3.50"
+        },
+        {
+          "name": "Coca-Cola zero 33 cl",
+          "price": " 3.50"
+        },
+        {
+          "name": "Fanta 33 cl",
+          "price": " 3.50"
+        },
+        {
+          "name": "Sprite 33 cl",
+          "price": " 3.50"
+        },
+        {
+          "name": "Amari",
+          "price": " 3.50"
+        },
+        {
+          "name": "Grappa",
+          "price": " 3.50"
+        },
+        {
+          "name": "Whiskey e Distillati",
+          "price": " 6.00"
+        }
+      ]
+      }
     };
   },
   created() {
-    this.fetchViniData();
+    //this.fetchViniData();
   },
   methods: {
-    async fetchViniData() {
-      try {
-        const response = await axios.get('https://johns-restaurant-todi-db.onrender.com/vini');
-        this.viniData = response.data[0];
-        console.log(this.viniData);
-      } catch (error) {
-        console.error('Error fetching vini data:', error);
-      }
-    }
+    // async fetchViniData() {
+    //   try {
+    //     const response = await axios.get('https://johns-restaurant-todi-db.onrender.com/vini');
+    //     this.viniData = response.data[0];
+    //     console.log(this.viniData);
+    //   } catch (error) {
+    //     console.error('Error fetching vini data:', error);
+    //   }
+    // }
   }
 };
 </script>
